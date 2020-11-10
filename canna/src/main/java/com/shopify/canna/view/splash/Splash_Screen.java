7e6a63a197ec8bb94059cc918530453282bc4bf0
manoj <manojshakya1207@gmail.com>
2020-11-10 10:16:47 +0530

    test

--- canna/src/main/java/com/shopify/canna/view/splash/Splash_Screen.java
+++ canna/src/main/java/com/shopify/canna/view/splash/Splash_Screen.java
@@ -1,14 +1,26 @@
 package com.shopify.canna.view.splash;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 
+import com.shopify.buy3.GraphCall;
+import com.shopify.buy3.GraphCallResult;
+import com.shopify.buy3.GraphError;
+import com.shopify.buy3.GraphResponse;
+import com.shopify.buy3.QueryGraphCall;
+import com.shopify.buy3.Storefront;
 import com.shopify.canna.R;
+import com.shopify.canna.SampleApplication;
+import com.shopify.canna.data.graphql.Converter;
 import com.shopify.canna.view.login.User_Login;
+
+import kotlin.Unit;
 
 import static androidx.core.os.HandlerCompat.postDelayed;
 
@@ -26,6 +38,8 @@ public class Splash_Screen extends AppCompatActivity {
                 Intent mainIntent = new Intent(Splash_Screen.this, User_Login.class);
                 startActivity(mainIntent);
                 finish();
+
+
             }
         }, 1000);
     }
